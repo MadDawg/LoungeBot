d464a5101df0d9c13635f0ab6bce82bea33ce8c2
Cornell Washington <cdwash1991@gmail.com>
2019-03-27 20:18:58 -0500

    ACTUALLY add date.js

---
+++ commands/date.js
@@ -0,0 +1,43 @@
+// TODO: allow default timezone to be set by admin
+
+module.exports = {
+    name: 'date',
+    aliases: [],
+    description: 'Show date/time',
+    guildOnly: false,
+    args: false,
+    usage: '[timezone]',
+    spammy: false,
+    admin: false,
+
+    execute(message, args, bot){
+        const format = {
+            timeZone: "UTC",
+            hourCycle: "h24",
+            weekday: "short",
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            timeZoneName: "short"
+        };
+
+        const date = new Date();
+
+        try{
+            if (args != "") format.timeZone = args;
+            message.channel.send(date.toLocaleString('en-GB', format));
+        }
+        catch(err) {
+            if (err instanceof RangeError){
+            //console.error(err);
+            //console.log("Defaulting to UTC");
+            message.channel.send(`Invalid or unsupported timezone: ${args}`);
+            format.timeZone = "UTC";
+            message.channel.send(date.toLocaleString('en-GB', format));
+            }
+        }
+    },
+}
