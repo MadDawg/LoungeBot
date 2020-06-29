814daa75be1fb905f70f44d662182b5ce7204bc5
Cornell Washington <cdwash1991@gmail.com>
2020-06-29 06:56:05 -0500

    finish/refactor (auto)sauce command, update README.md, remove python and zombie dependency

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # LoungeBot
 ### Dependencies
 - [Node.js](https://nodejs.org/) v8.0.0+
-- Python 3.4+
-  - [lxml](https://lxml.de/) v3.5.0+
-  - libxml2 v2.7.0+
-  - libxslt v1.1.23+
-  - pip/virtualenv
+~~- Python 3.4+~~
+  ~~- [lxml](https://lxml.de/) v3.5.0+~~
+  ~~- libxml2 v2.7.0+~~
+  ~~- libxslt v1.1.23+~~
+  ~~- pip/virtualenv~~
 
 ### Installation
 Clone the repository and run `npm install` in the project directory. After doing so, you will need to create a config.json file with the following fields:
--- loungebot.js
+++ loungebot.js
@@ -4,9 +4,11 @@
 
 const Discord = require('discord.js');
 const fs = require('fs');
-const Browser = require('zombie');
+//const Browser = require('zombie');
+const https = require('https');
 const parse = require('url-parse');
 const { spawn } = require('child_process');
+const http_status_codes = require('./resources/http_status_codes');
 
 // serverdb is the array of objects representing guild IDs
 // and their configured command prefixes
@@ -149,36 +151,157 @@ class LoungeBot{
         this.checkDB(guildid);
         return (this._serverdb.find(x => x.guildid === guildid).autosauce);
     }
-    constructSauceDispenser(json){
-        const items = JSON.parse(json);
-        if (!items.length) return undefined;
-
-        const best_guess = items[0];
-        //const embeds = [];
-
-        const len = best_guess.similarity.length;
-        const similarity = Number(best_guess.similarity.substring(0,len-1));
-        if (similarity < 85){
-            return;
-        }
-
-        const embed = new Discord.RichEmbed();
-        embed.setTitle('Best Guess for This Image');
-
-        if (best_guess.title != "")
-            embed.addField("Title", best_guess.title);
-        if (best_guess.creator != "")
-            embed.addField("Creator", best_guess.creator);
-
-        // should give original string on failure
-        embed.setURL(best_guess.imgurl.replace('&amp;', '&'));
-        embed.setThumbnail(best_guess.thumbnail.replace('&amp;', '&'));
-        embed.addField("Similarity", best_guess.similarity);
-        embed.setFooter("Sauce provided by SauceNao");
-
-        return embed;
+    // TODO: maybe move to another file
+    to_string(array){
+        if (!array.length) return "";
+        let str = "";
+        for (let i = 0; i < array.length; i++)
+            str += `${array[i]}\n`;
+        return str;
     }
-    getSauce(message){
+    constructSauceDispenser(results, indexes){
+        const embeds = [];
+
+        // fallback for status code mismatch
+        try{
+            !results[0];
+        }
+        catch(TypeError){
+            console.error("Somehow this error was not caught earlier...");
+            return [];
+        }
+        results.forEach((result, i) => {
+            //console.log(`Array index: ${i}`);
+            const similarity = parseFloat(result.header.similarity);
+            //if (similarity < minimum_similarity) continue;
+
+            const embed = new Discord.RichEmbed();
+            // add result header data
+            embed.setTitle("Best Guess for This Image");
+            embed.setThumbnail(result.header.thumbnail);
+            embed.addField("Similarity", `${result.header.similarity}%`); // remove if called by sauce command
+            embed.setURL(result.data.ext_urls[0]);
+
+            if (similarity > 69)
+                embed.setColor('#00FF00');
+            else if (similarity > 49)
+                embed.setColor('#FFFF00');
+            else
+                embed.setColor('#FF0000');
+
+            embed.setFooter("Sauce provided by SauceNao");
+            // add other result data
+            switch(result.header.index_id){
+              // don't even bother parsing the indexes object for pixiv
+              case 5:
+              case 6:
+              case indexes['drawr']:
+              case indexes['seiga']:
+              case indexes['nijie']:
+              case indexes['medibang']:
+              case indexes['bcyillust']:
+              case indexes['bcycosplay']:
+                embed.addField("Title", result.data.title);
+                embed.addField("Creator/Member", result.data.member_name);
+                break;
+
+              case indexes['deviantart']:
+                embed.addField("Title", result.data.title);
+                embed.addField("Author", result.data.author_name);
+                break;
+
+              case indexes['hmags']:
+                embed.addField("Title", result.data.title);
+                embed.addField("Episode/Part", result.data.part);
+                embed.addField("Date", result.data.date);
+                break;
+
+              case indexes['hcg']:
+                embed.addField("Title", result.data.title);
+                embed.addField("Company", result.data.company);
+                break;
+
+              case indexes['doujindb']:
+                break;
+
+              case indexes['hmisc']:
+                embed.addField("Title", result.data.source);
+                if (result.data.creator.length <= 5)
+                    embed.addField("Creator", this.to_string(result.data.creator));
+                else
+                    embed.addField("Creator", `${result.data.creator[0]} and ${result.data.creator.length} others`);
+
+                if (result.data.en_name)
+                    embed.addField("English Name", result.data.en_name);
+                if (result.data.jp_name)
+                    embed.addField("Japanese Name", result.data.jp_name);
+                break;
+
+              case indexes['2dmarket']:
+              case indexes['fakku']:
+                embed.addField("Source", result.data.source);
+                embed.addField("Creator", result.data.creator);
+                break;
+
+              // videos
+              case indexes['anime']:
+              case indexes['hanime']:
+              case indexes['shows']:
+              case indexes['movies']:
+                embed.addField("Source", result.data.source);
+                embed.addField("Year", result.data.year);
+                if (result.data.part)
+                    embed.addField("Episode/Part", result.data.part);
+                embed.addField("Estimated Time", result.data.est_time);
+                break;
+
+              case indexes['pawoo']:
+                embed.addField("User", result.data.pawoo_user_acct);
+                embed.addField("User Display Name", results.data.pawoo_user_display_name);
+                break;
+
+              case indexes['madokami']:
+              case indexes['mangadex']:
+                embed.addField("Source", result.data.source);
+                // can be tacky and would be annoying to fix.
+                embed.addField("Part/Chapter", result.data.source);
+                break;
+
+              // imageboards
+              case indexes['animepictures']:
+                // couldn't actually get a result that returned this site,
+                // but it looks like it'd yield the same results as the others
+                break;
+              case indexes['konachan']:
+              case indexes['idolcomplex']:
+              case indexes['danbooru']:
+              case indexes['gelbooru']:
+              case indexes['e621']:
+              case indexes['sankaku']:
+              case indexes['yandere']:
+                embed.addField("Creator", result.data.creator);
+                if (result.data.material)
+                    embed.addField("Material", result.data.material);
+                if (result.data.characters)
+                    embed.addField("Characters", result.data.characters);
+                break;
+
+              // could not find results that returned these;
+              // will need to ask staff for fields
+              case indexes['portalgraphics']:
+                break;
+              case indexes['shutterstock']:
+                break;
+              //case indexes['ALL']:
+              default:
+                console.log("how tho?");
+                break;
+            }
+            embeds.push(embed);
+        });
+        return embeds;
+    }
+    getSauce(message, numres=1){
         // this re expression may get recomplied every invokation?
         // https://urlregex.com/
         const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
@@ -188,52 +311,126 @@ class LoungeBot{
         //console.log(message.attachments);
 
         const urls = message.content.match(regex);
-        let url = "";
+        let imgurl = "";
         if (urls){
-            url = urls[0];
+            imgurl = urls[0];
         }
         else{
             try{
-                url = message.attachments.first().url;
+                imgurl = message.attachments.first().url;
             }
             catch(err){ return; }
         }
-        /*else if (message.attachments){
-            try{
-                url = message.attachments.first().url;
-            }
-            catch(err) return;
-        }
-        else return;*/
 
         // check if channel is NSFW and adjust SauceNao URL accordingly
         let hidelevel = "3";
         if (message.channel.nsfw){ hidelevel = "0"; }
-        const browser = new Browser();
-        function go(){
-            browser.assert.success();
-            browser.assert.text('title', 'Sauce Found?');
-            const html = browser.html('div.result');
+        //const dbmask = 0000000;
+        const db = 999; // will likely always be 999
+        const output_type = 2;
+        const testmode = 0;
+        //const numres = 1;
 
-            // parse html
-            const pyprocess = spawn('python3', ["htmlparser.py", html]);
+        // minimum similarity
+        // should be good enough for now
+        const minsim = "85!";
 
-            pyprocess.stdout.on('data', (data) => {
-                const json = data.toString();
+        //const imgurl = "https://cdn.discordapp.com/attachments/400205362369921024/726355980622888990/77718860_p0.jpg";
+        //const imgurl = "https://ehgt.org/g/blank.gif";
+        const url = `https://saucenao.com/search.php?db=${db}&hidelevel=${hidelevel}&output_type=${output_type}&testmode=${testmode}&numres=${numres}&minsim=${minsim}&url=${imgurl}`;
 
-                // construct and send message here!
-                const embed = this.constructSauceDispenser(json);
-                if (embed){ message.channel.send(embed); }
+        // broken indexes/indices included
+        const indexes = {
+            "hmags": 0, // deprecated
+            "hcg": 2, // deprecated
+            "doujindb": 3, // deprecated
+            "pixiv": [5,6], // this might be annoying to parse
+            "seiga": 8,
+            "danbooru": 9,
+            "drawr": 10, // site closed
+            "nijie": 11,
+            "yandere": 12,
+            "shutterstock": 15, // broken and disabled
+            "fakku": 16, // broken
+            "hmisc": 18,
+            "2dmarket": 19,
+            "medibang": 20, // broken
+            "anime": 21,
+            "hanime": 22, // not to be confused with hanime.tv
+            "movies": 23,
+            "shows": 24,
+            "gelbooru": 25,
+            "konachan": 26,
+            "sankaku": 27, // broken
+            "animepictures": 28,
+            "e621": 29,
+            "idolcomplex": 30, // somehow works even though it's actually a sankaku subdomain?
+            "bcyillust": 31, // broken
+            "bcycosplay": 32, // broken
+            "portalgraphics": 33, // site closed
+            "deviantart": 34, // broken
+            "pawoo": 35, // broken
+            "madokami": 36, // site down due to batato's death
+            "mangadex": 37,
+            "ALL": 999 // unlikely to appear in search results, but we'll include it anyway
+        }
 
+        https.get(url, (resp) => {
+            const status_msg = http_status_codes;
+            if (resp.statusCode !== 200){
+                if (resp.statusCode === 403){
+                    /*throw {
+                        name:        "BadSauceNAOAPIKey",
+                        message:     "Server recieved invalid API key",
+                        toString:    function(){return this.name + ": " + this.message;}
+                    };*/
+                    console.error("Server recieved invalid API key");
+                    return;
+                }
+                else{
+                    //throw new Error(`Server returned ${resp.statusCode} code`);
+                    console.error(`Server returned ${resp.statusCode} ${status_msg[String(resp.statusCode)]}`);
+                    return;
+                }
+            }
+
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
             });
-        }
-        try{
-            browser.visit(`http://saucenao.com/search.php?db=999&hide=${hidelevel}&url=${url}`, go.bind(this));
-        }
-        catch(AsssertionError){
-            console.error("SauceNao errored out.");
-            return;
-        }
+
+            // The whole response has been received.
+            resp.on('end', () => {
+                let results = [];
+                const header = JSON.parse(data).header;
+                //console.log(header.status);
+
+                if (header.status > 0){
+                    console.error("API Error. Try again later.");
+                    return;
+                }
+                else if (header.status < 0){
+                    if (header.message){
+                        console.error(`Search failed with message "${header.message}"`);
+                        return;
+                    }
+                    console.error("Bad image or other request error. Try a different image or try just again later.");
+                    return;
+                }
+
+                results = JSON.parse(data).results;
+
+                const embeds = this.constructSauceDispenser(results, indexes);
+                for (let i = 0; i < embeds.length; i++){
+                    message.channel.send(embeds[i]);
+                }
+              });
+
+        }).on("error", (err) => {
+            console.error("Error: " + err.message);
+        });
     }
 
     // --------------------------
