54ec6824a25e5689a5311b14732b757cc0b26278
Cornell Washington <cdwash1991@gmail.com>
2020-07-05 13:29:27 -0500

    add "use strict" lines that were accidentally removed

--- commands/echo.js
+++ commands/echo.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const Discord = require('discord.js');
 
 //TODO: implement soft character limit
--- commands/fbi.js
+++ commands/fbi.js
@@ -1,3 +1,6 @@
+"use strict";
+
+
 const fs = require('fs');
 const Discord = require('discord.js');
 
